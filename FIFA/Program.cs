802c8aa178f7bd5fe7a129a3473684fb522359bd
adam026 <76121613+adam026@users.noreply.github.com>
2022-05-16 00:14:01 +0200

    Juventus hozzáadása

---
+++ FIFA/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFA
+{
+    public class Csapat
+    {
+        public string Orszag { get; set; }
+        public int ReszvetelekSzama { get; set; }
+        public int LegelsoReszvetel { get; set; }
+        public int LegutobbiReszvetel { get; set; }
+        public string LegjobbEredmeny { get; set; }
+
+        public Csapat(string sor)
+        {
+            var buffer = sor.Split(';');
+
+            Orszag = buffer[0];
+            ReszvetelekSzama = int.Parse(buffer[1]);
+            LegelsoReszvetel = int.Parse(buffer[2]);
+            LegutobbiReszvetel = int.Parse(buffer[3]);
+            LegjobbEredmeny = buffer[4];
+        }
+    }
+    internal class Program
+    {
+        static List<Csapat> csapatok = new List<Csapat>();
+
+
+        static void Main(string[] args)
+        {
+            Beolvas();
+            F01();
+            F02();
+            F03();
+            F04();
+            F05();
+            F06();
+            F07();
+            F08();
+            Console.ReadKey();
+        }
+
+        private static void F08()
+        {
+
+            using (var sw = new StreamWriter(@"..\..\RES\legtobbszor.txt", false, Encoding.UTF8))
+            {
+                foreach (var csapat in csapatok)
+                {
+                    if (csapat.ReszvetelekSzama >= 10)
+                    {
+                        sw.WriteLine($"{csapat.Orszag}: {DateTime.Today.Year - csapat.LegelsoReszvetel}");
+                    }
+                }
+            }
+            Console.WriteLine("legtobbszor.txt kész!");
+        }
+
+        private static void F07()
+        {
+            var legelsoVB = csapatok.Min(x => x.LegelsoReszvetel);
+            Csapat magyarorszag = csapatok.Single(x => x.Orszag == "Magyarország");
+
+            if(magyarorszag.LegelsoReszvetel != legelsoVB)
+                Console.WriteLine("7. Feladat: Magyarország nem volt ott az első VB-n.");
+            else
+                Console.WriteLine("7. Feladat: Magyarország ott volt az első VB-n.");
+            Console.WriteLine();
+        }
+
+        private static void F06()
+        {
+            Console.WriteLine($"6. Feladat: Szlovákia legjobb eredménye: {csapatok.Single(x => x.Orszag == "Szlovákia").LegjobbEredmeny}");
+            Console.WriteLine();
+        }
+
+        private static void F05()
+        {
+            Console.WriteLine($"5. Feladat: Eddig {csapatok.Where(x => x.LegjobbEredmeny.Contains("Világbajnok")).Count()} ország csapata volt világbajnok");
+            Console.WriteLine();
+        }
+
+        private static void F04()
+        {
+            Console.WriteLine($"4. Feladat: Az első VB-t {csapatok.Min(x => x.LegelsoReszvetel)}-ban rendezték");
+            Console.WriteLine();
+        }
+
+        private static void F03()
+        {
+            int counter = 0;
+
+            foreach (var csapat in csapatok)
+            {
+                if (csapat.Orszag == "Belgium" || csapat.Orszag == "Hollandia" || csapat.Orszag == "Luxemburg")
+                    counter += csapat.ReszvetelekSzama;
+            }
+
+            Console.WriteLine($"3. Feladat: A BeNeLux országok összesen {counter} alkalommal vettek részt a VB-n");
+            Console.WriteLine();
+        }
+
+        private static void F02()
+        {
+            List<Csapat> ketezerTizennyolc = csapatok.Where(x => x.LegutobbiReszvetel == 2018).ToList();
+            Console.WriteLine("2. Feladat: 2018-as VB csapatai:");
+
+            for (int i = 1; i <= ketezerTizennyolc.Count; i++)
+            {
+                Console.Write("\t{0, -10}", ketezerTizennyolc[i - 1].Orszag);
+                if (i % 4 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+
+        }
+
+        private static void F01()
+        {
+            Console.WriteLine($"1. Feladat: Csapatok száma: {csapatok.Count}");
+            Console.WriteLine();
+        }
+
+        private static void Beolvas()
+        {
+            using (var sr = new StreamReader(@"..\..\RES\fociVBk.csv", Encoding.UTF8))
+            {
+                _ = sr.ReadLine();
+
+                while (!sr.EndOfStream)
+                {
+                    csapatok.Add(new Csapat(sr.ReadLine()));
+                }
+            }
+            
+        }
+    }
+}
---
+++ Juventus/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juventus
+{
+    public class Jatekos
+    {
+        public int Mezszam { get; set; }
+        public string JatekosNeve { get; set; }
+        public string JatekosNemzetisege { get; set; }
+        public string Poszt { get; set; }
+        public int SzuletesiEv { get; set; }
+
+        public Jatekos(string sor)
+        {
+            var buffer = sor.Split(';');
+            Mezszam = int.Parse(buffer[0]);
+            JatekosNeve = buffer[1];
+            JatekosNemzetisege = buffer[2];
+            Poszt = buffer[3];
+            SzuletesiEv = int.Parse(buffer[4]);
+        }
+    }
+    internal class Program
+    {
+        public static List<Jatekos> jatekosok = new List<Jatekos>();
+        public static Dictionary<string, int> posztokSzerint = new Dictionary<string, int>();
+        public static Dictionary<int, int> evekSzerint = new Dictionary<int, int>();
+        static void Main(string[] args)
+        {
+            Betolt();
+            F01();
+            F02();
+            F03();
+            F04();
+            F05();
+            F06();
+            F07();
+            F08();
+            F09();
+            F10();
+            Console.ReadKey();
+        }
+
+        private static void F10()
+        {
+            Console.WriteLine("10. Feladat: Fájlbaírás");
+        }
+
+        private static void F09()
+        {
+            Console.Write("9. Feladat: Kérek egy mezszámot: ");
+            var mezszam = int.Parse(Console.ReadLine());
+            var talalat = jatekosok.SingleOrDefault(x => x.Mezszam == mezszam);
+            if(talalat is null)
+                Console.WriteLine("\tNincs ilyen játékos!");
+            else
+                Console.WriteLine("\t" + talalat.JatekosNeve);
+        }
+
+        private static void F08()
+        {
+            Console.WriteLine("8. Feladat: Évek, amikor pontosan 3 játékos született:");
+            foreach (var jatekos in jatekosok)
+            {
+                if (!evekSzerint.ContainsKey(jatekos.SzuletesiEv))
+                    evekSzerint.Add(jatekos.SzuletesiEv, 1);
+                else
+                    evekSzerint[jatekos.SzuletesiEv]++;
+            }
+            var pontosanHarom = evekSzerint.Where(x => x.Value == 3).ToList();
+            foreach (var jatekos in pontosanHarom)
+            {
+                Console.WriteLine("\t" + jatekos.Key);
+            }
+            Console.WriteLine();
+        }
+
+        private static void F07()
+        {
+            Console.WriteLine($"7. Feladat: A legidősebb csatár: {jatekosok.Where(x => x.Poszt == "csatár").OrderByDescending(y => y.SzuletesiEv).Last().JatekosNeve}");
+            Console.WriteLine();
+        }
+
+        private static void F06()
+        {
+            Console.WriteLine("6. Feladat: ");
+            foreach (var jatekos in jatekosok)
+            {
+                if (!posztokSzerint.ContainsKey(jatekos.Poszt))
+                    posztokSzerint.Add(jatekos.Poszt, 1);
+                else
+                    posztokSzerint[jatekos.Poszt]++;
+            }
+
+            foreach (var poszt in posztokSzerint)
+            {
+                Console.WriteLine($"\t{poszt.Key} poszt : {poszt.Value} személy");
+            }
+            Console.WriteLine();
+        }
+
+        private static void F05()
+        {
+            Console.WriteLine($"5. Feladat: A csapat átlagéletkora {jatekosok.Average(x => DateTime.Today.Year - x.SzuletesiEv)} év");
+            Console.WriteLine();
+        }
+
+        private static void F04()
+        {
+            var legfiatalabb = jatekosok.OrderByDescending(x => DateTime.Today.Year - x.SzuletesiEv).Last();
+            Console.WriteLine($"4. Feladat: A legfiatalabb játékos neve: {legfiatalabb.JatekosNeve}");
+            Console.WriteLine();
+        }
+
+        private static void F03()
+        {
+            var counter = jatekosok.Where(x => x.JatekosNemzetisege == "olasz").Count();
+            Console.WriteLine($"3. Feladat: A csapatnak {counter} db Olasz nemzetiségű jétékosa van.");
+            Console.WriteLine();
+        }
+
+        private static void F02()
+        {
+            var counter = jatekosok.Where(x => x.JatekosNemzetisege == "magyar").Count();
+            if (counter != 0)
+                Console.WriteLine($"2. Feladat: A csapatnak {counter} db Magyar nemzetiségű játékosa van.");
+            else
+                Console.WriteLine("2. Feladat: A csapatnak nincs Magyar nemzetiségű játékosa.");
+            Console.WriteLine();
+        }
+
+        private static void F01()
+        {
+            Console.WriteLine($"1. Feladat: A csapatnak jelenleg {jatekosok.Count} igazolt játékosa van.");
+            Console.WriteLine();
+        }
+
+        private static void Betolt()
+        {
+            using (var sr = new StreamReader(@"..\..\RES\juve.txt", Encoding.UTF8))
+            {
+                while (!sr.EndOfStream)
+                {
+                    jatekosok.Add(new Jatekos(sr.ReadLine()));
+                }
+                
+            }
+        }
+    }
+}
